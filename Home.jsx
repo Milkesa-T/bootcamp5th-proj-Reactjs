81127d485cefdd2c05e49a296fc17fcb1771b936
Milkesa-T <milkesataye0450@gmail.com>
2025-12-30 20:04:23 +0300

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,17 +1,12 @@
-import { useTasks } from '../store/taskContext'; // Must have { }
+import TaskInput from "../components/TaskInput";
+import TaskList from "../components/TaskList";
 
-export default function Stats() {
-  const { tasks } = useTasks();
-  const completed = tasks.filter(t => t.completed).length;
-
+export default function Home() {
   return (
-    <div className="max-w-xl mx-auto p-6 bg-white dark:bg-gray-800 rounded-lg shadow mt-10">
-      <h2 className="text-2xl font-bold mb-4 dark:text-white text-center">Statistics</h2>
-      <div className="space-y-4 dark:text-gray-200">
-        <p className="flex justify-between">Total Tasks: <span>{tasks.length}</span></p>
-        <p className="flex justify-between">Completed: <span className="text-green-500">{completed}</span></p>
-        <p className="flex justify-between">Remaining: <span className="text-red-500">{tasks.length - completed}</span></p>
-      </div>
-    </div>
+    <main className="max-w-2xl mx-auto p-4">
+      <TaskInput />
+      <TaskList />
+    </main>
   );
 }
+console.log("HOME MOUNTED");
