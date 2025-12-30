7a7541ca6bc7fa7f0667c72d6e8fcede5a3f573d
Milkesa-T <milkesataye0450@gmail.com>
2025-12-30 20:08:26 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,13 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './styles/global.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App"; // Import the App component normally
+import { TaskProvider } from "./store/taskContext";
+import "./styles/global.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <TaskProvider>
+        <App />
+      </TaskProvider>
     </BrowserRouter>
   </StrictMode>
-)
+);
+
+ 
+ 
