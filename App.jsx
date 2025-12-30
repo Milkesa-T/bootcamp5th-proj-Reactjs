22f1d585c4f6537fab8a8ac6698a0697c4ca9638
Milkesa-T <milkesataye0450@gmail.com>
2025-12-30 20:03:17 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,27 +1,17 @@
-// src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { TaskProvider } from './store/taskContext';
-import Header from './components/Header';
-import Home from './pages/Home';
-import Stats from './pages/Stats';
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Stats from "./pages/Stats";
 
-function App() {
+export default function App() {
   return (
-    /* TaskProvider MUST wrap everything that uses the task state */
-    <TaskProvider> 
-      <Router>
-        <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors">
-          <Header />
-          <main className="container mx-auto p-4">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/stats" element={<Stats />} />
-            </Routes>
-          </main>
-        </div>
-      </Router>
-    </TaskProvider>
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/stats" element={<Stats />} />
+      </Routes>
+    </div>
   );
 }
-
-export default App;
